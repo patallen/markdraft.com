f5e7e59a1d5004666cbdf0e0aff031d7e2757524
Patrick Allen <prallen90@gmail.com>
2015-06-29 20:37:55 -0400

    Set up postgresql

--- markdraft/settings.py
+++ markdraft/settings.py
@@ -79,7 +79,11 @@ WSGI_APPLICATION = 'markdraft.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'markdraftdb',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
