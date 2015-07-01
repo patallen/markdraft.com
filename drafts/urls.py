8b5bced91836a9f159d8ebc1461196d394df8105
Patrick Allen <prallen90@gmail.com>
2015-06-30 20:58:42 -0400

    Use django-registration for registration form

--- drafts/urls.py
+++ drafts/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    url(r'^signup/$', views.SignupView.as_view(), name='signup')
+    url(r'^signup/$', views.SignupView.as_view(), name='signup'),
 ]
--- drafts/views.py
+++ drafts/views.py
@@ -3,9 +3,10 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from drafts.forms import SignupForm
+from registration.forms import RegistrationForm
 
 
 class SignupView(CreateView):
     template_name = 'signup.html'
-    form_class = SignupForm
+    form_class = RegistrationForm 
     success_url = '/'
