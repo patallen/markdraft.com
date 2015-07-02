acd063351fb51f8b732b1efbd8876981b9bb961b
Patrick Allen <prallen90@gmail.com>
2015-07-02 00:05:56 -0400

    Remove LoginView and it's references

--- drafts/views.py
+++ drafts/views.py
@@ -1,16 +1,5 @@
 from django.views.generic.edit import CreateView, FormView
-from drafts.forms import LoginForm
 from registration.forms import RegistrationForm
-
-
-class LoginView(FormView):
-    form_class = LoginForm
-    template_name = 'login.html'
-    success_url = '/'
-    
-    def form_valid(self, form):
-        print("cookies")
-        return super(LoginView, self).form_valid(form)
 
 
 class SignupView(CreateView):
