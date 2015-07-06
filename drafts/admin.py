344cd73cb9b32c7404872c4962809579cac9ff1a
Patrick Allen <prallen90@gmail.com>
2015-07-05 21:13:49 -0400

    Set up admin document view

--- drafts/admin.py
+++ drafts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from drafts.models import Document, Draft
 
-# Register your models here.
+
+class DraftInline(admin.TabularInline):
+    model = Draft
+    extra = 1
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['hashid', 'user']}),
+        ('Document Info',   {'fields': ['date_created', 'latest_draft']}),
+    ]
+    inlines = [DraftInline]
+    list_display = ('hashid', 'latest_draft', 'user', 'date_created')
+    readonly_fields = ['latest_draft', 'date_created']
+
+admin.site.register(Document, DocumentAdmin)
