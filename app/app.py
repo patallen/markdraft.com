c073d213816e417b754cf85b6498667f1fde1afb
Patrick Allen <prallen90@gmail.com>
2016-02-13 23:54:57 -0500

    Really set up docker...

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+#db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return "Hello!"
+    return "xx"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
