df6e7679aebded004cec703399b62ce7ee10c592
Patrick Allen <prallen90@gmail.com>
2016-02-15 02:38:04 -0500

    Add Share model

--- app/models/drafts.py
+++ app/models/drafts.py
@@ -1,5 +1,7 @@
 from models import db
 from models.mixins import AuditMixin, BaseMixin
+from uuid import uuid4
+from marklib.db import GUID
 
 
 class Document(AuditMixin, BaseMixin, db.Model):
@@ -19,3 +21,14 @@ class Draft(AuditMixin, BaseMixin, db.Model):
     version = db.Column(db.Integer, nullable=False)
     title = db.Column(db.String(120))
     body = db.Column(db.String())
+
+    def user_is_owner(self, user):
+        return self.document.user == user
+
+
+class Share(BaseMixin, db.Model):
+    uid = db.Column(GUID(), primary_key=True, default=uuid4)
+    document_uid = db.Column(db.Integer, db.ForeignKey('document.id'))
+    user_uid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    read = db.Column(db.Boolean, default=False, nullable=False)
+    write = db.Column(db.Boolean, default=False, nullable=False)
