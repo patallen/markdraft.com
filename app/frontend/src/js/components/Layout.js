f7c4e0d47a530dac922a1b709cb7214ef988d9a0
Patrick Allen <prallen90@gmail.com>
2016-02-23 07:19:52 -0500

    Set up basic react layout

---
+++ app/frontend/src/js/components/Layout.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./Header"
+import Footer from "./Footer"
+import Body from "./Body"
+
+export default class Layout extends React.Component {
+	render() {
+		return (
+			<div>
+				<Header/>
+				<Body/>
+				<Footer/>
+			</div>
+		)
+	}
+}
