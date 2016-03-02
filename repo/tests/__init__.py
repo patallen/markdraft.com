6f5eec90790371187b2dc47f463adbc74f61489c
Patrick Allen <prallen90@gmail.com>
2016-03-02 08:25:12 -0500

    Add self.app to BaseTestCase for testing views

--- repo/tests/__init__.py
+++ repo/tests/__init__.py
@@ -7,6 +7,7 @@ from api import config, app
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         app.config.from_object(config.Testing)
+        self.app = app.test_client()
         db.create_all()
         user = User(
             username="testuser",
@@ -25,3 +26,13 @@ class BaseTestCase(unittest.TestCase):
         for val in ours:
             if val not in theirs:
                 raise AssertionError("Expected %s in list." % (val,))
+
+    def assertStatus(self, response, expected):
+        if response.status_code != expected:
+            raise AssertionError(
+                "Expected status_code to be %s, got %s."
+                % (expected, response.status_code)
+            )
+
+    def assertStatus200(self, response):
+        self.assertStatus(response, 200)
