5137a29cda32f9512a4cf044491ef642e742b63d
Patrick Allen <prallen90@gmail.com>
2016-03-02 20:12:14 -0500

    Add tests for register and login user views

--- repo/api/tests/test_views.py
+++ repo/api/tests/test_views.py
@@ -1,5 +1,7 @@
-from tests import BaseTestCase
 import json
+
+from models import User
+from tests import BaseTestCase
 
 
 class UsersTestCase(BaseTestCase):
@@ -16,3 +18,34 @@ class UsersTestCase(BaseTestCase):
         results = json.loads(get.data).get('results')
         self.assertEqual(len(results), 1)
         self.assertNotAllowed("/users/1/documents", allowed=['GET'])
+
+    def test_auth_registration(self):
+        req = {
+            "email": "testacct@testing.com",
+            "username": "testing",
+            "password1": "testtest",
+            "password2": "testtest",
+            "first_name": "testy_user",
+            "last_name": "McUser"
+        }
+        res = self.app.post(
+            '/auth/register',
+            data=json.dumps(req),
+            headers=[('Content-Type', 'application/json')]
+        )
+        self.assertStatus200(res)
+        self.assertIsNotNone(User.query.filter_by(username="testing").first())
+
+    def test_auth_login(self):
+        req = json.dumps({
+            "username": "testuser",
+            "password": "123abc"
+        })
+        res = self.app.post(
+            '/auth/login',
+            data=req,
+            headers=[('Content-Type', 'application/json')]
+        )
+        self.assertStatus200(res)
+        results = json.loads(res.data).get('results')
+        self.assertAllIn(results.keys(), ['access_token'])
