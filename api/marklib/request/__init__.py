8ddee5ff66a7b22998fa867ef3d711795529dffd
Patrick Allen <prallen90@gmail.com>
2016-03-06 14:01:02 -0500

    Add status to MakeResponse

--- api/marklib/request/__init__.py
+++ api/marklib/request/__init__.py
@@ -8,16 +8,19 @@ class XHRResponse(Response):
 
 class MakeResponse(object):
     _response = XHRResponse()
+    res_dict = {}
 
     def __init__(self, status_code=None, body=None, error=None):
-        super(MakeResponse, self).__init__()
         self.response.set_data({})
+        self.res_dict = {
+            "status": "success",
+        }
         if status_code:
             self.set_status(status_code)
         if body:
             self.set_body(body)
         if error:
-            self.set_error(error=error)
+            self.set_error(msg=error)
 
     def add_header(self, header, override=True):
         for k, v in header.items():
@@ -28,21 +31,29 @@ class MakeResponse(object):
         self._response.headers[header] = value
 
     def set_body(self, data):
-        res = dict(results=data)
-        self._response.set_data(json.dumps(res, indent=4))
+        self.res_dict["results"] = data
+        if hasattr(self.res_dict, "error"):
+            self.res_dict.pop("error")
 
     def set_status(self, code=None, msg=None):
         self._response.status_code = code
+        if code < 200 or code >= 300:
+            self.res_dict["status"] = "error"
+        else:
+            self.res_dict["status"] = "success"
 
-    def set_error(self, code=None, error=None):
-        res = dict(error=error)
+    def set_error(self, code=None, msg=None):
+        self.res_dict["error"] = msg
+        self.res_dict["status"] = "error"
+        if hasattr(self.res_dict, 'results'):
+            self.res_dict.pop("results")
         if code:
             self.set_status(code)
-        self._response.set_data(json.dumps(res, indent=4))
 
     def set_content_type(self, content_type):
         self.set_header('Content-Type', content_type)
 
     @property
     def response(self):
+        self._response.set_data(json.dumps(self.res_dict, indent=4))
         return self._response
