bef7bbed29fb419576bad4846cbc91c0151a511e
Patrick Allen <prallen90@gmail.com>
2016-04-13 07:52:06 -0400

    Fix some NoneType bugs in helpers.format_result

--- api/api/helpers/__init__.py
+++ api/api/helpers/__init__.py
@@ -25,19 +25,25 @@ def filter_by_access(user, entities, permissions=None):
 
 
 def format_result(result, page=None, limit=None, count=None):
-    page, limit, count = [int(x) for x in (page, limit, count) if x]
+    for thing in (page, limit, count):
+        if thing is not None:
+            thing = int(thing)
 
-    next_page, prev_page = None, None
-    if limit * page < count:
+    next_page, prev_page, pages = None, None, None
+    if (limit and page) and limit * page < count:
         next_page = page + 1
     if page > 1:
         prev_page = page - 1
+    if (count and limit):
+        pages = math.ceil(count/limit)
+    else:
+        pages = 1
 
     pagination = dict(
         current=page,
         next=next_page,
         prev=prev_page,
-        pages=math.ceil(count/limit)
+        pages=pages
     )
     result = dict(
         pagination=pagination,
